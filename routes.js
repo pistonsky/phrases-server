3eb3f7cfd2484ade44534279a388a088a2e034b2
Aleksandr Tsygankov <tsygankov.aleksandr@gmail.com>
2017-09-03 16:58:46 +0700

    serve audio with audio/mpeg content type

--- routes.js
+++ routes.js
@@ -9,10 +9,10 @@ router.post('/upload', upload.any(), function(req, res, next) {
   res.json(req.files);
 });
 
-router.get('/phrase/(:id).caf', function(req, res){
-    res.set({'Content-Type': 'audio/x-caf'});
-    var readStream = fs.createReadStream('uploads/' + req.params.id);
-    readStream.pipe(res);
+router.get('/phrase/(:id)', function(req, res) {
+  res.set({ 'Content-Type': 'audio/mpeg' });
+  var readStream = fs.createReadStream('uploads/' + req.params.id);
+  readStream.pipe(res);
 });
 
 module.exports = router;

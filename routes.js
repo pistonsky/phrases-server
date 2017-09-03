f6f6f153ba8aa78ac8404637c99be36a0e6cdf3f
Aleksandr Tsygankov <tsygankov.aleksandr@gmail.com>
2017-09-03 15:44:05 +0700

    serve phrases separately with audio/x-caf content type

--- routes.js
+++ routes.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fs = require('fs');
 var multer = require('multer');
 var upload = multer({ dest: 'uploads/' });
 var router = express.Router();
@@ -6,6 +7,12 @@ var router = express.Router();
 /* POST saveblog router. */
 router.post('/upload', upload.any(), function(req, res, next) {
   res.json(req.files);
+});
+
+router.get('/phrase/(:id).caf', function(req, res){
+    res.set({'Content-Type': 'audio/x-caf'});
+    var readStream = fs.createReadStream('uploads/' + req.params.id);
+    readStream.pipe(res);
 });
 
 module.exports = router;

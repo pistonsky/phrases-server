f8667f9e8efd53addaf72b24968397b2c8c58f29
Aleksandr Tsygankov <tsygankov.aleksandr@gmail.com>
2017-09-11 03:20:07 +0800

    check for phrase existence, add share endpoint

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var fs = require('fs');
+var url = require('url');
 var multer = require('multer');
 var upload = multer({ dest: 'uploads/' });
 var router = express.Router();
@@ -10,9 +11,21 @@ router.post('/upload', upload.any(), function(req, res, next) {
 });
 
 router.get('/phrase/(:id)', function(req, res) {
-  res.set({ 'Content-Type': 'audio/mpeg' });
-  var readStream = fs.createReadStream('uploads/' + req.params.id);
-  readStream.pipe(res);
+  const uri = 'uploads/' + req.params.id;
+  fs.access(uri, e => {
+    if (e) {
+      res.status(404).send();
+    } else {
+      res.set({ 'Content-Type': 'audio/mpeg' });
+      var readStream = fs.createReadStream(uri);
+      readStream.pipe(res);
+    }
+  });
+});
+
+router.get('/share', function(req, res) {
+  const query = url.parse(req.url).query;
+  res.redirect('exp://exp.host/@pistonsky/phrases/+' + query);
 });
 
 module.exports = router;

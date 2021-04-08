825126b3d0cb056410b1fd4573a84714bb8d8baa
Aleksandr Tsygankov <tsygankov.aleksandr@gmail.com>
2021-04-09 01:37:57 +0300

    Temporarily remove everything except router

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var path = require('path');
 
 var app = express();
 
-app.use(express.json());
-app.use(express.static('public'));
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+// app.use(express.json());
+// app.use(express.static('public'));
+// app.use(express.static(path.join(__dirname, 'client', 'build')));
 app.use(require('./routes'));
 
 const PORT = process.env.PORT || 5000;

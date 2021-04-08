afab6efe5235c5da6f85188f82609368ba79ac73
Aleksandr Tsygankov <tsygankov.aleksandr@gmail.com>
2021-04-08 23:32:18 +0300

    Use PostgreSQL instead of MongoDB

--- routes.js
+++ routes.js
@@ -2,40 +2,31 @@ var express = require('express');
 var path = require('path');
 var url = require('url');
 var qs = require('qs');
-var mongoose = require('mongoose');
+const { Pool } = require('pg');
 var graph = require('fbgraph');
 var config = require('./config/keys');
 var { DEMO_USER_ID } = require('./config');
 var router = express.Router();
 
-require('./models/Phrases');
-require('./models/Users');
-
-mongoose.connect(config.mongoURI, {
-  server: {
-    auto_reconnect: true,
-    reconnectTries: Number.MAX_VALUE
-  }
-});
-
-const Phrase = mongoose.model('phrases');
-const Users = mongoose.model('users');
+const pool = new Pool({ connectionString: config.DATABASE_URL, ssl: { rejectUnauthorized: false } });
 
 const DEFAULT_DICTIONARY_NAME = 'Phrazes';
 
 async function sendPhrases({ user_id, dictionary, res }) {
-  const users = await Users.find({ user_id });
-  if (users.length === 0) {
-    await new Users({ user_id }).save();
+  const result = await pool.query('select * from users where id = $1', [user_id]);
+  if (result.rows.length === 0) {
+    await pool.query('insert into users (id) values ($1)', [user_id]);
   }
   let conditions = { user_id };
-  if (dictionary) conditions.dictionary = dictionary;
-  const phrases = await Phrase.find(conditions);
+  const conditionsQuery = ['user_id = $1'];
+  if (dictionary) {
+    conditions.dictionary = dictionary;
+    conditionsQuery.push('dictionary = $2');
+  }
+  const phrasesResult = await Pool.query(`select id, user_id, dictionary, language, original, translated, uri from phrases where ${conditionsQuery.join(' and ')}`, dictionary ? [user_id, dictionary] : [user_id]);
   res.json({
     ...conditions,
-    phrases: phrases.map(item => {
-      return Object.assign({ id: item._id }, item._doc);
-    })
+    phrases: phrasesResult.rows,
   });
 }
 
@@ -148,19 +139,32 @@ router.post('/', async (req, res) => {
     translated,
     uri
   } = req.query;
-  const users = Users.find({ user_id });
-  if (users.length === 0) {
-    await new Users({ user_id }).save();
+  const users = await Pool.query('select id from users where id = $1', [user_id]);
+  if (users.rows.length === 0) {
+    await Pool.query('insert into users (id) values ($1)', [user_id]);
   }
   try {
-    let keys_to_update = {};
-    if (original) keys_to_update.original = original;
-    if (translated) keys_to_update.translated = translated;
-    if (dictionary) keys_to_update.dictionary = dictionary;
-    if (language) keys_to_update.language = language;
-    const updateResult = await Phrase.updateMany({ user_id, uri }, { $set: keys_to_update });
-    if ((updateResult.ok === 1) && (updateResult.nModified > 0)) {
-      res.status(200).json({ updated: updateResult.nModified });
+    const values = [user_id, uri];
+    const columns = [];
+    if (original) {
+      columns.push(`original = $${3 + columns.length}`);
+      values.push(original);
+    }
+    if (translated) {
+      columns.push(`translated = $${3 + columns.length}`);
+      values.push(translated);
+    }
+    if (dictionary) {
+      columns.push(`dictionary = $${3 + columns.length}`);
+      values.push(dictionary);
+    }
+    if (language) {
+      columns.push(`language = $${3 + columns.length}`);
+      values.push(language);
+    }
+    const updateResult = await Pool.query(`update phrases set ${columns.join(', ')} where user_id = $1 and uri = $2`, values);
+    if (updateResult.rowCount > 0) {
+      res.status(200).json({ updated: updateResult.rowCount });
     } else {
       const phrase = {
         user_id,
@@ -170,9 +174,9 @@ router.post('/', async (req, res) => {
         translated,
         uri
       };
-      const insertResult = await new Phrase(phrase).save();
-      if (insertResult) {
-        res.status(200).json(insertResult);
+      const insertResult = await Pool.query(`insert into phrases (user_id, dictionary, language, original, translated, uri) values ($1, $2, $3, $4, $5, $6)`, [user_id, dictionary || DEFAULT_DICTIONARY_NAME, language, original, translated, uri]);
+      if (insertResult.rowCount > 0) {
+        res.status(200).json(phrase);
       } else {
         res.status(500).json({ error: 'Could not save phrase', phrase });
       }
@@ -186,8 +190,13 @@ router.post('/', async (req, res) => {
 router.delete('/', async (req, res) => {
   const { uri } = req.query;
   const err = await Phrase.deleteOne({ uri });
-  if (err) res.json(err);
-  else res.json({});
+  try {
+    await Pool.query('delete from phrases where uri = $1', [uri]);
+    res.status(200).json({});
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({});
+  }
 });
 
 router.get('/share', function(req, res) {
@@ -197,37 +206,38 @@ router.get('/share', function(req, res) {
 
 router.delete('/dictionary', async (req, res) => {
   const { id, user_id } = req.query;
-  const error = await Phrase.deleteMany({ user_id, dictionary: id });
-  if (error) res.status(500).json({ error });
-  else res.json({});
+  try {
+    await Phrase.deleteMany({ user_id, dictionary: id });
+    await Pool.query('delete from phrases where user_id = $1 and dictionary = $2', [user_id, id]);
+    res.status(200).json({});
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({});
+  }
 });
 
 router.put('/dictionary', async (req, res) => {
   const { old_name, new_name, user_id } = req.query;
   try {
-    const result = await Phrase.updateMany({ user_id, dictionary: old_name }, { $set: { dictionary: new_name }});
-    if (result.ok === 1) {
-      res.status(200).json({ updated: result.nModified });
-    } else {
-      res.status(500).json(result);
-    }
+    const result = await Pool.query('update phrases set dictionary = $1 where dictionary = $2', [new_name, old_name]);
+    res.status(200).json({ updated: result.rowCount });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ error });
+    res.status(500).json({});
   }
 });
 
 router.get('*', async (req, res) => {
   const query = qs.parse(req.params[0].replace('/phrazesapp://+', ''));
   if (query.dictionary) {
-    const count = await Phrase.count({ user_id: query.user_id, dictionary: query.dictionary });
+    const result = await Pool.query('select count(*) as count from phrases where user_id = $1 and dictionary = $2', [query.user_id, query.dictionary]);
     res.status(200).send(`
       <html>
         <head>
           <title>${query.dictionary}</title>
-          <meta name="description"  content="Learn ${count} phrases now in Phrazes app!" />
+          <meta name="description"  content="Learn ${result.rows[0].count} phrases now in Phrazes app!" />
           <meta property="og:title" content="${query.dictionary}" />
-          <meta property="og:description" content="Learn ${count} phrases now in Phrazes app!" />
+          <meta property="og:description" content="Learn ${result.rows[0].count} phrases now in Phrazes app!" />
           <meta property="og:image" content="https://pistonsky-phrases.herokuapp.com/phrazes.png" />
           <meta property="og:image:width" content="1024" />
           <meta property="og:image:height" content="1024" />
@@ -260,14 +270,14 @@ router.get('*', async (req, res) => {
         </html>
       `);
     } else {
-      const count = await Phrase.count({ user_id: query.user_id });
+      const result = await Pool.query('select count(*) as count from phrases where user_id = $1', [query.user_id]);
       res.status(200).send(`
         <html>
           <head>
             <title>Phrazes: speak like a local!</title>
             <meta property="og:title" content="Phrazes: speak like a local!" />
-            <meta name="description"  content="Learn these ${count} phrases now in Phrazes app!" />
-            <meta property="og:description" content="Learn these ${count} phrases now in Phrazes app!" />
+            <meta name="description"  content="Learn these ${result.rows[0].count} phrases now in Phrazes app!" />
+            <meta property="og:description" content="Learn these ${result.rows[0].count} phrases now in Phrazes app!" />
             <meta property="og:image" content="https://pistonsky-phrases.herokuapp.com/phrazes.png" />
             <meta property="og:image:width" content="1024" />
             <meta property="og:image:height" content="1024" />
